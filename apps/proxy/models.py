434039d4589dbed5d279ae74a10bc72e87a7aa9d
ehco1996 <zh19960202@gmail.com>
2020-11-16 11:51:11 +0800

    fix: fix cache connector

--- apps/proxy/models.py
+++ apps/proxy/models.py
@@ -515,13 +515,13 @@ class UserOnLineIpLog(BaseLogModel):
         return f"{self.proxy_node.name}用户在线IP记录"
 
     @classmethod
-    def get_recent_log_by_node_id(cls, node_id):
+    def get_recent_log_by_node_id(cls, proxy_node):
         # TODO 优化一下IP的存储方式
         now = utils.get_current_datetime()
         ip_set = set()
         ret = []
         for log in cls.objects.filter(
-            node_id=node_id,
+            proxy_node=proxy_node,
             created_at__range=[now.subtract(seconds=c.NODE_TIME_OUT), now],
         ):
             if log.ip not in ip_set:
